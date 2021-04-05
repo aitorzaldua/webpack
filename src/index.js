494718ab346fac335bcc41f231277b4b255d2ce5
Aitor Zaldua <aitor.zaldua@gmail.com>
2021-04-05 18:03:45 +0200

    last of april 5th

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import sum from './utils/sum.js';
+
+
+const hello = 'hello';
+
+console.log (hello);
+console.log(sum(2,2));
+
---
+++ src/utils/sum.js
@@ -0,0 +1,7 @@
+const sum = (a, b) => {
+    return a + b;
+}
+
+export default sum
+
+//exportamos la variable para poder usarla en otro script
